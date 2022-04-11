d0f7a39075fea5504c4fcff7c717d5e7ffede014
Chris Sterritt <csterritt@gmail.com>
2022-04-10 20:49:13 -0400

    First script that shows just the number of days.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
-
 <template>
   <div
     class="flex flex-row items-center min-h-16 md:mt-2 mb-4 md:mx-4 shadow-lg bg-secondary text-neutral-content rounded-box"
@@ -15,9 +10,21 @@
   <div class="card w-96 bg-base-200 shadow-xl mx-6 w-fit">
     <div class="card-body">
       <h2 class="card-title">Time until August 24, 2022:</h2>
-      <p>About four or five months (?)</p>
+      <p>About {{ days }} days.</p>
     </div>
   </div>
 </template>
 
-<style></style>
+<script setup>
+const then = new Date()
+then.setFullYear(2022)
+then.setMonth(7)
+then.setDate(24)
+then.setHours(13)
+const now = new Date()
+let days = `Now in the past!`
+if (now.getMonth() <= 7 && now.getDate() <= 23) {
+  const delta = then.getTime() - now.getTime()
+  days = Math.round(delta / (1000 * 60 * 60 * 24))
+}
+</script>
