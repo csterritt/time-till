e7ec8c2499e2e52051d0de5643c09349a516b2ff
Chris Sterritt <csterritt@gmail.com>
2022-04-10 20:23:18 -0400

    Initial DaisyUI layout.

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,20 @@
 </script>
 
 <template>
-  <h2>Time until 8/24/2022!</h2>
+  <div
+    class="flex flex-row items-center min-h-16 md:mt-2 mb-4 md:mx-4 shadow-lg bg-secondary text-neutral-content rounded-box"
+  >
+    <div class="flex-1 px-2 mx-2">
+      <span class="text-lg font-bold">Time until</span>
+    </div>
+  </div>
+
+  <div class="card w-96 bg-base-200 shadow-xl mx-6 w-fit">
+    <div class="card-body">
+      <h2 class="card-title">Time until August 24, 2022:</h2>
+      <p>About four or five months (?)</p>
+    </div>
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['autumn', 'forest', 'cmyk'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'forest',
+  },
 }
